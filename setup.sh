ecf7acee4a16b5431c7d43fb8de004b091b7e3b2
Julio Henriquez <julio.m.henriquez@gmail.com>
2022-04-11 20:27:38 -0400

    Adds newgrp

--- setup.sh
+++ setup.sh
@@ -15,6 +15,8 @@ sudo usermod -a -G microk8s $USER
 
 sudo chown -f -R $USER ~/.kube
 
+newgrp microk8s
+
 echo "Restarting session for $USER. Password might be required."
 
 su - $USER -w COMPANY_DOMAIN -c cd $(pwd) && ./complete-setup.sh
