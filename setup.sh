cd49bd5a0f12d65032e1302a27c6a669aa22eda5
Jorge Cuesta <jorge@poktscan.com>
2022-06-01 23:02:52 +0000

    remove cpx; remove complete-setup.sh call in setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,4 @@ sudo chown -f -R $USER ~/.kube
 
 newgrp microk8s
 
-echo "Restarting session for $USER. Password might be required."
-
-su - $USER -w COMPANY_DOMAIN -c cd $(pwd) && ./complete-setup.sh
+echo "Restart session for $USER. Password might be required."
